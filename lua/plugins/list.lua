9895853686d4087ffe35515231275070610a9b72
colspan <collin@collindigm.com>
2024-02-04 21:32:12 +0300

    Telescope added into the plugin list

---
+++ lua/plugins/list.lua
@@ -0,0 +1,42 @@
+local function load_config(package)
+    return function() require('plugins.' .. package) end
+end
+
+return {
+	-- UI
+	
+	
+	-- Language
+	
+	
+	-- Treesitter
+	
+
+	-- LSP
+	 
+
+	-- Completion
+	 
+
+	-- Tools
+	 
+
+	-- Telescope
+	'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+            },
+            'nvim-telescope/telescope-symbols.nvim',
+            'molecule-man/telescope-menufacture',
+            'debugloop/telescope-undo.nvim',
+            'ThePrimeagen/harpoon',
+        },
+        config = load_config('tools.telescope'),
+        cmd = 'Telescope', 
+
+	-- Git
+}
