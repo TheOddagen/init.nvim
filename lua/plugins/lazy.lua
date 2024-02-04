e2d0c76fa772f4a30612da5aa72599a35dce3d34
colspan <collin@collindigm.com>
2024-02-05 02:28:51 +0300

    Update the colorscheme from onedark to gruvbox

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -51,7 +51,7 @@ lazy.setup({
         -- install missing plugins on startup. This doesn't increase startup time.
         missing = true,
         -- try to load one of these colorschemes when starting an installation during startup
-        colorscheme = { 'onedark' },
+        colorscheme = { 'gruvbox' },
     },
     ui = {
         -- a number <1 is a percentage., >1 is a fixed size
