829268ffb78838b31f787b3307922200fe27b485
colspan <collin@collindigm.com>
2024-02-12 16:11:35 +0300

    More options initiated for Behaviour and Appearance

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -11,15 +11,20 @@ opt.wrap = false					-- Display lines as one long line
 opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
-opt.hlsearch = false
+opt.hlsearch = false				-- highlight all matches on previous search pattern
 
 
 -- Appearance
 opt.number = true					-- Set numbered lines
 opt.relativenumber = true 			-- Set relative numbered lines
 opt.termguicolors = true
+opt.scrolloff = 10
 
 
 -- Behaviour
 opt.clipboard:append('unnamedplus')	-- allows neovim to access the system clipboard
+opt.mouse:append('a') 				-- allow the mouse to be used in neovim
 opt.cursorline = true				-- highlight the current line
+opt.encoding = 'UTF-8'
+opt.fileencoding = 'UTF-8'
+opt.confirm = true 					-- Confirm to save changes before exiting modified buffer
