d1145dce39d3d9d1075572ef72ec93e9d6c4453d
colspan <collin@collindigm.com>
2024-03-08 00:04:06 +0300

    native lsp clean-up

--- lua/plugins/lang/lsp-zero.lua
+++ lua/plugins/lang/lsp-zero.lua
@@ -10,7 +10,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-		ensure_installed = {},
+		ensure_installed = {'tsserver', 'gopls', 'jdtls',},
 		handlers = {
 				lsp_zero.default_setup,
 				lua_ls = function()
@@ -25,6 +25,22 @@ require('mason-lspconfig').setup({
 						}
 						lua_opts.settings.Lua.format = custom_options
 						require('lspconfig').lua_ls.setup(lua_opts)
+						require('lspconfig').gopls.setup({
+								on_attach = function ()
+										capabilities = capabilities
+										vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0}) 			-- 0 means whenver the current buffer is
+										vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+										vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = 0})
+										vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
+										-- vim.keymap.set("n", "gr", vim.lsp.buf.reference, {buffer = 0})			-- goto reference
+										vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
+										vim.keymap.set("n", "<leader>db", vim.diagnostic.goto_prev, {buffer = 0})
+										vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer = 0})
+										vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer = 0}) 		-- run :wa for Write All because it changed it in another buffer
+										vim.keymap.set("n", "<leader>i", vim.lsp.buf.code_action, {buffer = 0}) 		-- run :wa for Write All because it changed it in another buffer
+										-- Telescope lsp_references
+								end
+						})
 				vim.diagnostic.config({
 						update_in_insert = true,
 						float = {
@@ -39,3 +55,4 @@ require('mason-lspconfig').setup({
 		end,
 		},
 })
+
